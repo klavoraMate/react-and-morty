fe8ad19240bae9615320b236c2adc37386dbac91
Golen Gerhárd <golengerhard@gmail.com>
2022-12-13 14:33:21 +0100

    feat: Create description structure

--- react-and-morty/src/components/descriptions/CharacterDescription.js
+++ react-and-morty/src/components/descriptions/CharacterDescription.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
-const CharacterDescription = ({characters}) => {
-    const {id} = useParams();
-    console.log(characters[Number(id)-1]);
+const CharacterDescription = ({ characters }) => {
+  const { id } = useParams();
+  const character = characters[Number(id) - 1]
   return (
-    <div>CharacterDescription</div>
+    <div>
+      <h2>{character.name}</h2>
+      <img src={character.image} alt={character.name} />
+      <h3>Origin: {character.origin.name}</h3>
+      <p>Species: {character.species}</p>
+      <p> Gender: {character.gender}</p>
+      <h4>Location: {character.location.name}</h4>
+      {character.type && <p>Type: {character.type}</p>}
+
+    </div>
   )
 }
 
--- react-and-morty/src/components/descriptions/LocationDescription.js
+++ react-and-morty/src/components/descriptions/LocationDescription.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
-const LocationDescription = ({locations}) => {
-    const {id} = useParams();
-    console.log(locations[Number(id)-1]);
+const LocationDescription = ({ locations }) => {
+  const { id } = useParams();
+  const location = locations[Number(id) - 1];
   return (
-    <div>LocationDescription</div>
+    <div>
+      <h2>{location.name}</h2>
+      <h3>Origin: {location.dimension}</h3>
+      {location.type && <p>Type: {location.type}</p>}
+    </div>
   )
 }
 
