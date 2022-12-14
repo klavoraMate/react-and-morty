74ed906e5d2e8be51ff443fdf7dd3b9a554d6c3c
Golen Gerhárd <golengerhard@gmail.com>
2022-12-14 10:02:03 +0100

    feat: Set visibility of description

--- react-and-morty/src/components/Home.js
+++ react-and-morty/src/components/Home.js
@@ -1,22 +1,23 @@
 import "./Home.css"
 import { useNavigate } from "react-router-dom"
-
+import { useState } from "react"
 
 const Home = () => {
   const navigate = useNavigate()
+  const [descriptionVisibility, setDescriptionVisibility] = useState(true);
+
   const navigateTo = (urlEnd) => {
-    // 👇️ navigate to /contacts
+    setDescriptionVisibility(false);
     navigate(urlEnd);
   };
 
 
   return (
     <div className="Home">
-      <img src="/images/Daco_4305710.png" alt="React And Morty" />
-      <img src="/images/Daco_4407915.png" alt="Rect and Morty group" />
-      <p>Description:
-        On this website you can see characters and locations of the famous series React and Morty!
-      </p>
+      <img src="/images/Daco_4305710.png" alt="React And Morty" className="r-a-m" />
+      <img src="/images/Daco_4407915.png" alt="Rect and Morty group" className="logo" />
+      {descriptionVisibility && <p>Description: On this website you can see characters and locations of the famous series React and Morty!
+      </p>}
       <div>
         <button onClick={() => navigateTo('./characters')}>Characters</button>
         <button onClick={() => navigateTo('./locations')}>Locations</button>
