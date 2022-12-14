6a20653761e581acec6f2e3b901eaeb8d530f929
Golen Gerhárd <golengerhard@gmail.com>
2022-12-14 08:59:49 +0100

    feat: Create basic homepage

--- react-and-morty/src/components/Home.js
+++ react-and-morty/src/components/Home.js
@@ -1,7 +1,27 @@
+import "./Home.css"
+import { useNavigate } from "react-router-dom"
+
 
 const Home = () => {
+  const navigate = useNavigate()
+  const navigateTo = (urlEnd) => {
+    // 👇️ navigate to /contacts
+    navigate(urlEnd);
+  };
+
+
   return (
-    <div>Home</div>
+    <div className="Home">
+      <img src="/images/Daco_4305710.png" alt="React And Morty" />
+      <img src="/images/Daco_4407915.png" alt="Rect and Morty group" />
+      <p>Description:
+        On this website you can see characters and locations of the famous series React and Morty!
+      </p>
+      <div>
+        <button onClick={() => navigateTo('./characters')}>Characters</button>
+        <button onClick={() => navigateTo('./locations')}>Locations</button>
+      </div>
+    </div>
   )
 }
 
