75e9f26726b632a00adeee43820e5180ec64a74c
Golen Gerhárd <golengerhard@gmail.com>
2022-12-14 14:33:28 +0100

    fix: stlye added to characterlist

--- react-and-morty/src/components/cards/CharacterList.js
+++ react-and-morty/src/components/cards/CharacterList.js
@@ -2,15 +2,16 @@ import Card from "./Card"
 import { useParams } from 'react-router-dom';
 import useFetch from "../../api/useFetch";
 import { mainUrls } from "../../api/dataRoutes";
+import "../../style/cards.css"
 
 const CharacterList = () => {
   const { id } = useParams();
   const { data: characters } = useFetch(mainUrls.characters + id);
   if (characters) {
     return (
-      <div>
+      <div className="character-list">
         {characters.results.map((character) => (
-          <div key={character.id} >
+          <div className="card" key={character.id} >
             <Card id={character.id} img={character.image} name={character.name} identity="character" />
           </div>
         ))}
