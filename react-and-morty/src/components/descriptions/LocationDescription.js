3193c5c8227e1d88826e1fb74c695735011ab8eb
Golen Gerhárd <golengerhard@gmail.com>
2022-12-14 17:26:09 +0100

    style: Add style to character description

--- react-and-morty/src/components/descriptions/LocationDescription.js
+++ react-and-morty/src/components/descriptions/LocationDescription.js
@@ -2,19 +2,21 @@ import React from 'react'
 import useFetch from '../../api/useFetch';
 import { mainUrls } from '../../api/dataRoutes';
 import { useParams } from 'react-router-dom';
+import "../../style/description.css"
+
 
 const LocationDescription = () => {
-    const {id} = useParams();
-    const { data: location } = useFetch(mainUrls.location + id);
-    if(location){
-      return (
-        <div>
-          <h2>{location.name}</h2>
-          <h3>Origin: {location.dimension}</h3>
-          {location.type && <p>Type: {location.type}</p>}
-        </div>
-      )
-    }
+  const { id } = useParams();
+  const { data: location } = useFetch(mainUrls.location + id);
+  if (location) {
+    return (
+      <div>
+        <h2>{location.name}</h2>
+        <h3>Origin: {location.dimension}</h3>
+        {location.type && <p>Type: {location.type}</p>}
+      </div>
+    )
+  }
 
 }
 
