536b64d79e306056a9709ca2c3b78464ff4d4e84
Golen Gerhárd <golengerhard@gmail.com>
2022-12-15 18:43:31 +0100

    fix: fix home page description text

--- react-and-morty/src/components/cards/CardList.js
+++ react-and-morty/src/components/cards/CardList.js
@@ -2,7 +2,7 @@ import Card from "./Card"
 import { useRef, useCallback } from "react";
 
 
-const CardList = ({ identities, hasMore, loading, handleSetPageNumber , url,style}) => {
+const CardList = ({ identities, hasMore, loading, handleSetPageNumber, url, style }) => {
   const observer = useRef();
   const lastCardRef = useCallback(lastCard => {
     if (loading) return
@@ -17,18 +17,18 @@ const CardList = ({ identities, hasMore, loading, handleSetPageNumber , url,styl
   }, [loading, hasMore, handleSetPageNumber])
   if (identities.length) {
     return (
-      <div className="character-list">
+      <div className="list">
         {identities.map((identity, index) => {
           if (identities.length === index + 1) {
             return (
               <div className="card" key={identity.id} ref={lastCardRef} >
-                <Card  img={identity.image} name={identity.name} style={style} url={url+identity.id}/>
+                <Card img={identity.image} name={identity.name} style={style} url={url + identity.id} />
               </div>
             )
           }
           return (
             <div className="card" key={identity.id} >
-              <Card img={identity.image} name={identity.name} style={style} url={url+identity.id}/>
+              <Card img={identity.image} name={identity.name} style={style} url={url + identity.id} />
             </div>
           )
         })}
