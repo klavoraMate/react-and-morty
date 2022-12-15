714ce218c48715c07b26d0587e6c9f1247cf8953
Golen Gerhárd <golengerhard@gmail.com>
2022-12-15 19:15:52 +0100

    fix: Fix scroll to end of list crash webpage

--- react-and-morty/src/api/useFetchList.js
+++ react-and-morty/src/api/useFetchList.js
@@ -15,13 +15,10 @@ const useFetch = (url) => {
     fetch(url, { signal })
       .then((response) => response.json())
       .then((fetchedData) => {
-        console.log(fetchedData);
-        if (fetchedData.results) {
-          setHasMore(fetchedData.results.length > 0)
-          setLoading(false)
-          return setData(previousData => [...previousData, ...fetchedData.results])
-        }
-        return setData(fetchedData);
+        console.log('FETCHED DATA LENGTH: ', fetchedData.results.length)
+        setHasMore(fetchedData.results.length > 0)
+        setLoading(false)
+        return setData(previousData => [...previousData, ...fetchedData.results])
       })
       .catch((err) => {
         setLoading(false)
@@ -30,6 +27,7 @@ const useFetch = (url) => {
           console.log('There are no more pages')
           setHasMore(false)
           console.log(err)
+
         }
 
       })
