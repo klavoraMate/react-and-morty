701967440890b9a5133c5984bf2e38ca72c91b50
klavoraMate <klavoramate98@gmail.com>
2023-09-07 09:57:49 +0200

    feat: converted to TS Card component

---
+++ react-and-morty/src/components/cards/Card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {mainUrls} from '../../api/dataRoutes';
+
+import '../../style/cards.css';
+
+interface CardProps {
+    img: string;
+    name: string;
+    url: string;
+    style?: React.CSSProperties;
+}
+
+const Card: React.FC<CardProps> = ({img, name, url, style}) => {
+    const imgSrc = typeof img === 'string' ? img : mainUrls.planetImg;
+
+    return (
+        <Link to={url}>
+            <div className='inner-card'>
+                <h3>{name}</h3>
+                <img
+                    src={imgSrc}
+                    alt={name}
+                    style={style}
+                />
+            </div>
+        </Link>
+    );
+};
+
+export default Card;
